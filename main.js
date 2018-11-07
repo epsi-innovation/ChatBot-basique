59dc8c30a9da149f9976f9fc285173433e331e17
Pierre Boucher <Pierre Boucher@DESKTOP-MH1KFIO>
2018-11-07 14:45:33 +0100

    chatBot avec tableau

--- main.js
+++ main.js
@@ -18,12 +18,20 @@ recursiveAsyncReadLine();
 function verifTheme(phrase) {
     var reponseRetournee = null;
     
-    if(phrase.indexOf('meteo') >= 0){
-      reponseRetournee = 'il fait beau';
-    }else if(phrase.indexOf('heure') >= 0){
-      reponseRetournee = 'il est 13h';
-    }else{
-      reponseRetournee = 'pourrais-tu reformuler ?';
+    var meteo = ['temps', 'météo'];
+    var heure = ['heure', 'horloge'];
+    
+    var words = phrase.split(' ');
+    
+    for(var i=0;i<words.length;i++){
+        if(meteo.indexOf(words[i]) >= 0){
+            reponseRetournee = 'il fait beau';
+        }else if(heure.indexOf(words[i]) >= 0){
+            reponseRetournee = 'il est 13h';
+        }
+    }    
+    if(reponseRetournee===null){
+        reponseRetournee='pourrais-tu reformuler ?';
     }
     return reponseRetournee;
 }
